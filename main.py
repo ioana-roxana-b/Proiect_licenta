95185229af5fdbc83147dfa9eb724bb63b967808
Roxana <ioana.boriceanu@gmail.com>
2023-06-11 12:23:43 +0300

    test rfe

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import additional_functions as adf
 import run
+import time
 import generate_dataset
 if __name__ == '__main__':
 
-    adf.delete_files('Results')
-    run.test()
+    #adf.delete_files('Results')
+    start_time = time.time()
+    run.test_rfe()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
+    print("Elapsed time: {} seconds".format(elapsed_time))
 
 
 
